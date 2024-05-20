714035a13e3fb455eceae916e24def32212843fd
paulalussana <2306404@ucc.edu.ar>
2024-05-20 08:45:04 -0300

    Se hizo la refactorizacion de conversor de unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -4,30 +4,39 @@
  * @param {string} nombre -id del elemento en html
  * @param {number} valor -valor ingresado por el usuario
  */
-function convertirUnidades(nombre, valor){
+convertirUnidades = (nombre, valor) =>{
+    let valMetro, valPulgada, valPie, valYarda;
     if (isNaN(valor)){
         alert("El valor ingresado no es un numero");
-        document.getElementById("metro").value = "";
-        document.getElementById("pulgada").value = "";
-        document.getElementById("pie").value = "";
-        document.getElementById("yarda").value = "";
+        valMetro = " ";
+        valPulgada = " ";
+        valPie = " ";
+        valYarda = " ";
     }else if (nombre=="metro"){
-        document.getElementById("pulgada").value = valor*39.3701;
-        document.getElementById("pie").value = valor*3.28084;
-        document.getElementById("yarda").value = valor*1.09361;
+        valMetro = valor;
+        valPulgada = valor*39.3701;
+        valPie = valor*3.28084;
+        valYarda = valor*1.09361;
     }else if (nombre=="pulgada"){
-        document.getElementById("metro").value = valor/39.37;
-        document.getElementById("pie").value = valor/12;
-        document.getElementById("yarda").value = valor/36;
+        valPulgada = valor;
+        valMetro = valor/39.37;
+        valPie = valor/12;
+        valYarda = valor/36;
     }else if (nombre=="pie"){
-        document.getElementById("pulgada").value = valor/3;
-        document.getElementById("metro").value = valor/3.281;
-        document.getElementById("yarda").value = valor/3;
+        valPie = valor;
+        valPulgada = valor/3;
+        valMetro = valor/3.281;
+        valYarda = valor/3;
     }else if (nombre=="yarda"){
-        document.getElementById("pulgada").value = valor*36;
-        document.getElementById("pie").value = valor*3;
-        document.getElementById("metro").value = valor/1.094;
+        valYarda = valor;
+        valPulgada = valor*36;
+        valPie = valor*3;
+        valMetro = valor/1.094;
     }
+    document.getElementById("metro").value = valMetro;
+    document.getElementById("pulgada").value = valPulgada;
+    document.getElementById("pie").value = valPie;
+    document.getElementById("yarda").value = valYarda;
 }
 
 function convertirGR(id){
